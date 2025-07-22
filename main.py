34a3aed8a7865ca9b365bfcc2c28bd4d5d40f61c
Danadis <dhund.data@gmail.com>
2025-07-22 22:47:37 +0100

    preparing player, asteroid shapes and collission

--- main.py
+++ main.py
@@ -3,19 +3,24 @@ from constants import *
 
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock =    pygame.time.Clock()
+    dt = 0
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    while True:
+    running = True
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
         screen.fill(0)
         pygame.display.flip()
+        pygame.tick(60)
 
     
 if __name__ == "__main__":
