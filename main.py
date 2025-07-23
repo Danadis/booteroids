478ab8ca85f4679ac214f6045bfc351d792353ae
Danadis <dhund.data@gmail.com>
2025-07-24 00:18:59 +0100

    rome wasn't built in a day

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    clock =    pygame.time.Clock()
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    # delta time for fps
     dt = 0
 
     print("Starting Asteroids!")
@@ -18,9 +22,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
-        screen.fill(0)
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-        pygame.tick(60)
+
+        # 60 fps
+        dt = clock.tick(60) / 1000
 
     
 if __name__ == "__main__":
